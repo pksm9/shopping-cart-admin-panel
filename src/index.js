034317955736a8b291e738edb70c039e2990456d
Your Name <sandaruwanprabod@gmail.com>
2024-01-24 09:14:34 +0530

    Dashboard layout added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from "@apollo/client";
+import {onError} from "@apollo/client/link/error"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const errorLink = onError(({graphqlErrors, networkErrors}) => {
+  if(graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({uri: "http://155.248.246.152:8080/graphql"})
+]);
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Image, Layout, Menu, theme } from 'antd';
 
-export default function DashBoard() {
-    return (
-        <div>
-            <h1>Dash Board</h1>
-        </div>
-    )
+const { Header, Content, Footer, Sider } = Layout;
+
+function getItem(label, key, icon, children) {
+  return {
+    key, icon, children, label,
+  };
 }
+
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [getItem('Tom', '3'), getItem('Bill', '4'), getItem('Alex', '5'), ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+];
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {token: { colorBgContainer, borderRadiusLG },} = theme.useToken();
+  
+  return (
+    <Layout style={{height: '100vh',}}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Image src="/logo192.png" width="80%" preview={false} style={{ margin: '20px 0', objectFit: 'scale-down' }} /></div>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Header style={{padding: 0, background: colorBgContainer, }} />
+        <Content style={{ margin: '24px 16px 0', }} >
+          <div style={{padding: 24, height: '100vh', background: colorBgContainer, borderRadius: borderRadiusLG,}}>
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer></Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;

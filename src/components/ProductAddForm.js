31c41d84174f2e839b6d175ef59130fa5b12eb6a
Your Name <sandaruwanprabod@gmail.com>
2024-01-29 15:15:55 +0530

    Add Product form UI completed

--- src/components/ProductAddForm.js
+++ src/components/ProductAddForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, Form, Input, InputNumber } from 'antd';
+import { Button, Col, Form, Input, Row, TreeSelect } from 'antd';
 
-const layout = {labelCol: {span: 4,},wrapperCol: {span: 16, },};
+const layout = {labelCol: {span: 24,},wrapperCol: {span: 20, }, size:"medium"};
 
 /* eslint-disable no-template-curly-in-string */
 const validateMessages = {
@@ -19,23 +19,45 @@ const onFinish = (values) => {
 };
 
 const ProductAddForm = () => (
-  <Form {...layout} name="nest-messages" onFinish={onFinish} style={{maxWidth: 600,}} validateMessages={validateMessages}>
-    <Form.Item name={['user', 'name']} label="Name" rules={[{required: true,},]} >
+  <Form {...layout} name="nest-messages" onFinish={onFinish} style={{maxWidth: 580, marginLeft: '30px', marginTop: '10px'}}  validateMessages={validateMessages} >
+    <Form.Item name={['product', 'name']} label="Product Name" rules={[{required: true,},]}  style={{ marginBottom: '6px' }} >
       <Input />
     </Form.Item>
-    <Form.Item name={['user', 'email']} label="Email" rules={[{type: 'email',},]}>
+    <Row gutter={0}>
+      <Col span={11}>
+      <Form.Item name={['product', 'code']} label="Product Code" rules={[{required: true,},]} style={{ marginBottom: '6px' }}>
       <Input />
     </Form.Item>
-    <Form.Item name={['user', 'age']} label="Age" rules={[{type: 'number',min: 0,max: 99,},]} >
-      <InputNumber />
-    </Form.Item>
-    <Form.Item name={['user', 'website']} label="Website">
+      </Col>
+      <Col span={11}>
+      <Form.Item name={['product', 'price']} label="Unit Price" rules={[{required: true,},]} style={{ marginBottom: '6px' }}>
       <Input />
     </Form.Item>
-    <Form.Item name={['user', 'introduction']} label="Introduction">
-      <Input.TextArea />
+      </Col>
+    </Row>
+    <Form.Item name={['product', 'brand']} label="Brand" rules={[{required: true,},]} style={{ marginBottom: '6px' }}>
+      <Input />
     </Form.Item>
-    <Form.Item wrapperCol={{...layout.wrapperCol, offset: 8,}}>
+    <Form.Item name={['product', 'category']} label="Category" rules={[{required: true,},]} style={{ marginBottom: '6px' }}>
+    <TreeSelect
+          treeData={[
+            {
+              title: 'Light',
+              value: 'light',
+              children: [
+                {
+                  title: 'Bamboo',
+                  value: 'bamboo',
+                },
+              ],
+            },
+          ]}
+        />
+    </Form.Item>
+    <Form.Item name={['product', 'introduction']} label="Introduction" rules={[{required: true,},]} style={{ marginBottom: '30px' }}>
+      <Input.TextArea autoSize={{ minRows: 3, maxRows: 3 }}/>
+    </Form.Item>
+    <Form.Item wrapperCol={{...layout.wrapperCol, offset: 0,}} >
       <Button type="primary" htmlType="submit">
         Submit
       </Button>
